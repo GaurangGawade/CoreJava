fe49174ed9b26c819c860b01f1e43ac26572ee6f
admin <admin@DESKTOP-7QJAOHR>
2024-04-01 13:28:08 +0530

    Jsp&SonarLint

---
+++ SonarLintDemo/src/com/edubridge/sonarlintdemo/Hello.java
@@ -0,0 +1,12 @@
+package com.edubridge.sonarlintdemo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+			String name="Gaurang";
+			int age=22;
+			System.out.println(name+" "+age);
+
+	}
+
+}
